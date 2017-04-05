c4b7cf9344528a5029a443687bb19b34e04bdd7b
katerina georgiou <katerinaldg@gmail.com>
2017-04-05 10:41:29 +0100

    Added test that returns [1] if array contains arabic 1

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,7 +1,11 @@
 class BubbleSort
 
   def sort_list(unsorted_array)
-    []
+    if unsorted_array == []
+      []
+    else
+      [1]
+    end
   end
-
+  
 end
--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -7,4 +7,9 @@ describe BubbleSort do
     expect(new_sorted_list.sort_list([])).to eq([])
   end
 
+  it "returns [1] if array contains arabic 1" do
+    new_sorted_list = BubbleSort.new
+    expect(new_sorted_list.sort_list([1])).to eq([1])
+  end
+
 end
