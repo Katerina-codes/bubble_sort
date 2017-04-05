3d04b4ccf7a751557ed60996d72104554f4809e1
katerina georgiou <katerinaldg@gmail.com>
2017-04-05 13:18:20 +0100

    Refactored code to make it less specific

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,19 +1,16 @@
 class BubbleSort
 
-
   def sort_list(unsorted_array)
 
-    if unsorted_array == []
-      []
-    elsif unsorted_array == [1]
-      [1]
-    elsif unsorted_array == [1, 2]
-      [1, 2]
-    elsif unsorted_array == [2, 1]
-      [1, 2]
+    if unsorted_array.length <= 1
+      unsorted_array
+    elsif unsorted_array[0] > unsorted_array[1]
+      unsorted_array[0], unsorted_array[1] = unsorted_array[1], unsorted_array[0]
     else
-      [-1]
+      unsorted_array[0] < unsorted_array[1]
+      unsorted_array
     end
   end
+
 
 end
