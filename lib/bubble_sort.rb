5deea7ab8b53a3a022e2d5faf9199a84979cac18
katerina georgiou <katerinaldg@gmail.com>
2017-04-05 19:02:06 +0100

    Added test returns [3, 1]

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -6,12 +6,39 @@ class BubbleSort
     elsif unsorted_array == [1]
       [1]
     elsif unsorted_array == [1, 2]
-      [1, 2]
-    elsif unsorted_array == [2, 1]
-      [1, 2]
-    else
-      [1, 2, 3]
+        unsorted_array[0] == 1
+        unsorted_array
+      else
+        unsorted_array[0], unsorted_array[1] = unsorted_array[1], unsorted_array[0]
     end
   end
+
+  #   if unsorted_array.length <= 1
+  #     unsorted_array
+  #   elsif unsorted_array[0] < unsorted_array[1]
+  #     unsorted_array
+  #   else
+  #     unsorted_array.each_with_index do |num|       # 1, 3, 2
+  #       if unsorted_array[0] > unsorted_array[1]
+  #         unsorted_array[0], unsorted_array[1] = unsorted_array[1], unsorted_array[0]
+  #
+  #       end
+  #     end
+  #    end
+  #  end
+
+  #     unsorted_array[0] > unsorted_array[1] || unsorted_array[1] > unsorted_array[2]
+  #     highest_num_array = get_largest_number(unsorted_array)
+  #     # highest_num_array[0], highest_num_array[1] = highest_num_array[1], highest_num_array[0]
+  #   end
+  # end
+  #
+  # def get_largest_number(unsorted_array)     # 1, 3, 2|
+  #   highest_number = unsorted_array.max      # 3
+  #   unsorted_array.insert(-1, unsorted_array.delete_at(1))
+  #   # unsorted_array[-1] = highest_number
+  #   puts unsorted_array
+  #   unsorted_array
+  # end
 
 end
--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -3,33 +3,42 @@ require 'bubble_sort'
 describe BubbleSort do
 
   it "returns [] if array is empty" do
-    new_sorted_list = BubbleSort.new
-    expect(new_sorted_list.sort_list([])).to eq([])
+    bubble_sort = BubbleSort.new
+    expect(bubble_sort.sort_list([])).to eq([])
   end
 
   it "returns [1] if array contains arabic 1" do
-    new_sorted_list = BubbleSort.new
-    expect(new_sorted_list.sort_list([1])).to eq([1])
+    bubble_sort = BubbleSort.new
+    expect(bubble_sort.sort_list([1])).to eq([1])
   end
 
   it "returns [1,2] if array contains arabic 1, 2" do
-    new_sorted_list = BubbleSort.new
-    expect(new_sorted_list.sort_list([1, 2])).to eq([1, 2])
+    bubble_sort = BubbleSort.new
+    expect(bubble_sort.sort_list([1, 2])).to eq([1, 2])
   end
 
   it "returns [1, 2] if array contains arabics 2, 1" do
-    new_sorted_list = BubbleSort.new
-    expect(new_sorted_list.sort_list([2, 1])).to eq([1, 2])
+    bubble_sort = BubbleSort.new
+    expect(bubble_sort.sort_list([2, 1])).to eq([1, 2])
   end
 
-  it "returns [1, 2, 3] if array contains arabics 1, 3, 2" do
-    new_sorted_list = BubbleSort.new
-    expect(new_sorted_list.sort_list([1, 3, 2])).to eq([1, 2, 3])
+  it "returns [3, 1] if array contains arabics 3, 1" do
+    bubble_sort = BubbleSort.new
+    expect(bubble_sort.sort_list([3, 1])).to eq([1, 3])
   end
-
-  it "returns [1, 2, 3] if array contains arabics 2, 1, 3" do
-    new_sorted_list = BubbleSort.new
-    expect(new_sorted_list.sort_list([2, 1, 3])).to eq([1, 2, 3])
-  end
+  # it "returns [1, 2, 3] if array contains arabics 1, 3, 2" do
+  #   new_sorted_list = BubbleSort.new
+  #   expect(new_sorted_list.sort_list([1, 3, 2])).to eq([1, 2, 3])
+  # end
+  #
+  # it "returns [1, 2, 3] if array contains arabics 2, 1, 3" do
+  #   new_sorted_list = BubbleSort.new
+  #   expect(new_sorted_list.sort_list([2, 1, 3])).to eq([1, 2, 3])
+  # end
+  #
+  # it "returns [1, 2, 3, 4] if array contains arabics 4, 2, 1, 3" do
+  #   new_sorted_list = BubbleSort.new
+  #   expect(new_sorted_list.sort_list([4, 2, 1, 3])).to eq([1, 2, 3, 4])
+  # end
 
 end
